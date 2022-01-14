6db0a31ef429ee0bfee37bb001166c9973f56de2
ahrodriguez1163 <65744425+ahrodriguez1163@users.noreply.github.com>
2022-01-14 09:50:39 -0500

    conf gitnore

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps : [{
+    script: './dist/server.js',
+    instances : 4,
+    exec_mode : "cluster",
+    env: {
+      PORT: "3002"
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'SSH_USERNAME',
+      host : 'SSH_HOSTMACHINE',
+      ref  : 'origin/master',
+      repo : 'GIT_REPOSITORY',
+      path : 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
